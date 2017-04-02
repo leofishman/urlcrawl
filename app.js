d7de178b94ab48281be7bc172d2dc0fa061b0d7e
leofishman <leofishman@gmail.com>
2017-04-02 13:41:51 -0300

    node yargs module, definition of crawl commando and url, depth parameters

--- app.js
+++ app.js
@@ -4,6 +4,31 @@ console.log('starting url crawl on:');
 // in order to check validities of url, spetialy the seed
 var validUrl = require('valid-url');
 
+// yargs to read commands and parameters
+var argv = require('yargs')
+	.command('crawl', 'Create a new crawl seed', function (yargs) {
+		yargs.options({
+			url: {
+				demand: true,
+				alias: 'u',
+				description: 'url to crawl, must begin with http(s)://',
+				type: 'string'
+			},
+			depth: {
+				demand: false,
+				default: 3,
+				alias: 'd',
+				description: 'how deep shall we go nesting from the original usrl seed? (default = 3)',
+				type: 'number'
+			}			
+		}).help('help');
+	})
+	.help('help')
+	.argv;
+var command = argv._[0];
+
+
+
 function check_url_valid(url2check) {
     if (validUrl.isUri(url2check)){
         return true;
@@ -12,6 +37,20 @@ function check_url_valid(url2check) {
     }
 }
 
+if (command === 'crawl') {
+	var url2seed = argv.url;
+	if (check_url_valid(url2seed)) {
+		console.log('crwal started at: ' + url2seed + ' ' + argv.depth + ' levels deep');
+	} else {
+		console.log(url2seed + ' is in invalid url');
+	} 
+} else {
+		console.log('command not found!');
+		console.log(command);
+}
+
+
+/*
 var url2check = "https://google";
 
 if (check_url_valid(url2check)) {
@@ -19,12 +58,8 @@ if (check_url_valid(url2check)) {
 } else {
 	console.log(url2check + ' is an invalid url');
 }
+* */
 
 // we let the user know the app finished
 console.log('thanks for using urlcrawl');
-
-
-
-
-
 
