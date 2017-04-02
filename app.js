3989e076a7407cbc8ff939ebad7f13833a9b7b8b
leofishman <leofishman@gmail.com>
2017-04-02 16:06:08 -0300

    first scrap try, links on console.log

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// we let the user know the app started
-console.log('starting url crawl on:');
-var error = {};
-error.error = false
+var request = require('request'),
+	cheerio = require('cheerio');
+
 // in order to check validities of url, spetialy the seed
 var validUrl = require('valid-url');
 
@@ -28,7 +27,10 @@ var argv = require('yargs')
 	.argv;
 var command = argv._[0];
 
+var error = {};
+error.error = false;
 
+var links = [];
 
 function check_url_valid(url2check) {
     if (validUrl.isUri(url2check)){
@@ -40,10 +42,38 @@ function check_url_valid(url2check) {
     }
 }
 
+function url_seed(url2scrap) {
+	request({url: url2scrap, encoding: 'binary'}, function(err, resp, body){
+		if(!err && resp.statusCode == 200){
+			var $ = cheerio.load(body);
+			$('a').each(function(){
+				//console.log($(this).attr('href'));
+				link = {'url': $(this).attr('href'),'father_url':url2scrap, 'deep': depth};
+				if (depth < argv.depth) {
+					depth++;
+					url_seed(link.url);
+				} 
+				links.push(link);
+				console.log(link);
+			});
+			console.log(links);
+		} else {
+			error.error = true;
+			error.description = url2scrap + ':: ' + err.message;
+		}
+	});
+}
+ 
+
 if (command === 'crawl') {
 	var url2seed = argv.url;
 	if (check_url_valid(url2seed)) {
+		// we let the user know the app started
 		console.log('crwal started at: ' + url2seed + ' ' + argv.depth + ' levels deep');
+		// create the first link in the links array
+		depth = 0;
+		var link = {'url': url2seed,'description':'we start crawling from here','father_url':url2seed,'deep':depth};
+		links.push(link);
 	} else {
 		//console.log(url2seed + ' is in invalid url');
 		console.log(error);
@@ -54,20 +84,11 @@ if (command === 'crawl') {
 		console.log(error);
 }
 
-
-/*
-var url2check = "https://google";
-
-if (check_url_valid(url2check)) {
-	console.log(url2check + ' is a valid url');
-} else {
-	console.log(url2check + ' is an invalid url');
-}
-* */
-
- 
-// we let the user know the app finished
+// we let the user know the app finished as soon as we can make it work asyncronic
 if (!error.error) {
+	url_seed(url2seed);
+	//console.log(links);
+	//console.log(link);
 	console.log('thanks for using urlcrawl');
 } else {
 	console.log('sorry, we couldn\'t crawl the url: ' +  url2seed + ' because ' + error.description);
