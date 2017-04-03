4233a8afe9ac0fe4d087a1f9739de2657288d2cf
leofishman <leofishman@gmail.com>
2017-04-02 22:08:59 -0300

    title and description for links

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ var error = {};
 error.error = false;
 
 var links = [];
+var link = {};
 
 function check_url_valid(url2check) {
     if (validUrl.isUri(url2check)){
@@ -42,37 +43,48 @@ function check_url_valid(url2check) {
     }
 }
 
-function url_seed(url2scrap) {
+function url_seed(url2scrap, urlFather, depth) {
+	//console.log(url2scrap);
 	request({url: url2scrap, encoding: 'binary'}, function(err, resp, body){
 		if(!err && resp.statusCode == 200){
+			
 			var $ = cheerio.load(body);
+			var title = $('title').text();
+			var description;
+			var meta = $('meta')
+			var keys = Object.keys(meta)
+			keys.forEach(function(key){	
+				try {
+					if (meta[key].attribs.name === 'description') {
+					  console.log(meta[key].attribs.content);
+					  description = meta[key].attribs.content;
+					}
+				}
+				catch (e) {
+				}
+			});
+			
+			link = {'url': url2scrap,'title':title,'description':description,'father_url':urlFather,'deep':depth};
+			links.push(link);
+			//console.log(link);
+			
 			$('a').each(function(){
-				//console.log($(this).attr('href'));
-				link = {'url': $(this).attr('href'),'father_url':url2scrap, 'deep': depth};
-				if (depth < argv.depth ) { // TODO add a condition to check if the url is already in the links array to avoid infinit recursive loops
-					depth++;
-					url_seed(link.url);
-				} 
-				links.push(link);
+				var next_link = $(this).attr('href');
+				//link = {'url': $(this).attr('href'),'father_url':url2scrap, 'deep': depth};
 				
+				if (depth <= argv.depth ) { // TODO add a condition to check if the url is already in the links array to avoid infinit recursive loops
+					depth++;
+					console.log(next_link + urlFather + depth);
+					url_seed(next_link, urlFather,depth);
+				} 
 			});
 			// console.log(links);
 		} else {
 			error.error = true;
-			error.description = url2scrap + ':: ' + err.message;
+			error.description = url2scrap + ':: ' + err.message; // TODO check err message or resp.status
 		}
 		//console.log(link);
-		var json2csv = require('json2csv');
-		var fields = ['url', 'description','father_url','deep'];
-		 
-		try {
-		  var result = json2csv({ data: links, fields: fields });
-		  console.log(result);
-		} catch (err) {
-		  // Errors are thrown for bad options, or if the data is empty and no fields are provided. 
-		  // Be sure to provide fields if it is possible that your data array will be empty. 
-		  console.error(err);
-		}
+
 	});
 }
  
@@ -82,10 +94,7 @@ if (command === 'crawl') {
 	if (check_url_valid(url2seed)) {
 		// we let the user know the app started
 		console.log('crwal started at: ' + url2seed + ' ' + argv.depth + ' levels deep');
-		// create the first link in the links array
-		depth = 0;
-		var link = {'url': url2seed,'description':'we start crawling from here','father_url':url2seed,'deep':depth};
-		links.push(link);
+		url_seed(url2seed, url2seed, 0);
 	} else {
 		//console.log(url2seed + ' is in invalid url');
 		console.log(error);
@@ -98,11 +107,25 @@ if (command === 'crawl') {
 
 // we let the user know the app finished as soon as we can make it work asyncronic
 if (!error.error) {
-	url_seed(url2seed);
+
+	setTimeout(function() {
+		print_csv();
+		console.log('thanks for using urlcrawl');
+		}, 6000);
 	//console.log(links);
 	//console.log(link);
-	console.log('thanks for using urlcrawl');
+
 } else {
 	console.log('sorry, we couldn\'t crawl the url: ' +  url2seed + ' because ' + error.description);
 }
 
+function print_csv() {
+		var fields = ['url', 'title','description','father_url','deep'];
+		var json2csv = require('json2csv');
+		try {
+		  var result = json2csv({ data: links, fields: fields });
+		  console.log(result);
+		} catch (err) {
+		  console.error(err);
+		}	
+}
